fb97a203a7458a96be460536ff7ab05bbd8a3125
Soufiane El marsi <sel-mars@e2r4p10.1337.ma>
2023-03-08 11:55:39 +0100

    serv-ok

--- test.cpp
+++ test.cpp
@@ -25,22 +25,29 @@
 
 class ft_irc {
   private:
+	char *_buff;
 	class client {
-	  private:
-		pollfd		_pfd;
-		std::string _name;
-
 	  public:
+		std::string _nickname;
+		std::string _msg_in;
+		std::string _msg_out;
 		client( void ) {}
-		client( const pollfd &pfd_, const std::string &name_ ) : _pfd( pfd_ ), _name( name_ ) {}
+	};
+	class channel {
+	  public:
+		std::string _name;
+		channel( void ) {}
 	};
 
   public:
-	std::string			  in_port;
-	std::string			  welcome_msg;
-	std::vector< client > _clients;
-	std::vector< pollfd > _pfds;
+	std::string			   in_port;
+	std::string			   password;
+	std::string			   welcome_msg;
+	std::vector< channel > _channels;
+	std::vector< client >  _clients;
+	std::vector< pollfd >  _pfds;
 	ft_irc( void ) {
+		_buff = new char[ 512 ];
 		welcome_msg += "\n";
 		welcome_msg += "\033[2m";
 		welcome_msg += "Greetings and welcome to our internet relay chat server !\n";
@@ -50,6 +57,10 @@ class ft_irc {
 		welcome_msg += "and we're sure that you'll fit right in.\n";
 		welcome_msg += "\033[22m";
 		welcome_msg += "\n";
+	}
+	~ft_irc( void ) {
+		delete[] _buff;
+		_buff = NULL;
 	}
 	void parse_args( int ac, char **av ) {
 		int port_nb;
@@ -66,6 +77,7 @@ class ft_irc {
 		in_port = ss.str();
 		ss.str( std::string() );
 		ss.clear();
+		password = av[ 2 ];
 	}
 	void acceptClient( void ) {
 		pollfd pfd;
@@ -74,30 +86,29 @@ class ft_irc {
 		if ( pfd.fd == -1 ) ERRNO_EXCEPT;
 		pfd.events = POLLIN | POLLOUT;
 		_pfds.push_back( pfd );
-		_clients.push_back( client( pfd, "" ) );
+		_clients.push_back( client() );
 		std::cout << "\033[2m"
 				  << "New client connected"
 				  << "\033[22m\n";
-		_pfds.back().revents = POLLOUT;
-		sendMsg( _pfds.back(), welcome_msg );
+		_clients.back()._msg_out = welcome_msg;
 	}
-	void sendMsg( pollfd &client, std::string &msg ) {
-		int bytes_sent = ( client.revents & POLLOUT ? 0 : -1 ), bytes_to_send = msg.length();
-		while ( bytes_sent != -1 && bytes_sent != bytes_to_send )
-			bytes_sent = send( client.fd, msg.c_str() + bytes_sent, bytes_to_send - bytes_sent, 0 );
+	void disconClient( const int client_pos ) {
+		close( _pfds[ client_pos + 1 ].fd );
+		_pfds.erase( _pfds.begin() + client_pos + 1 );
+		_clients.erase( _clients.begin() + client_pos );
+	}
+	void sendMsg( const int client_pos ) {
+		if ( _clients[ client_pos ]._msg_out.empty() ) return;
+		int bytes_sent = send( _pfds[ client_pos + 1 ].fd, _clients[ client_pos ]._msg_out.c_str(),
+							   _clients[ client_pos ]._msg_out.length(), 0 );
 		if ( bytes_sent == -1 ) THROW_EXCEPT( "Unable to send() data to client" );
+		_clients[ client_pos ]._msg_out.erase( 0, bytes_sent );
 	}
-	void recvMsg( pollfd &client ) {
-		char		buff[ 10 ];
-		std::string msg( 10, '\0' );
-		int			bytes_recvd = 0;
-		while ( bytes_recvd != -1 && msg.back() != '\n' ) {
-			bzero( buff, 10 );
-			bytes_recvd = recv( client.fd, buff, 9, 0 );
-			msg.append( buff );
-		}
-		if ( bytes_recvd == -1 ) THROW_EXCEPT( "Unable to recv() data from client" );
-		sendMsg( client, msg );
+	void recvMsg( const int client_pos ) {
+		bzero( _buff, 1000 );
+		int bytes_rcvd = recv( _pfds[ client_pos + 1 ].fd, _buff, 999, 0 );
+		if ( bytes_rcvd == -1 ) THROW_EXCEPT( "Unable to recv() data from client" );
+		_clients[ client_pos ]._msg_in.append( _buff, bytes_rcvd );
 	}
 };
 
@@ -131,9 +142,13 @@ int main( int ac, char **av ) {
 		while ( 1 ) {
 			if ( poll( &irc._pfds.front(), irc._pfds.size(), -1 ) == -1 ) ERRNO_EXCEPT;
 			if ( irc._pfds.front().revents == POLLIN ) irc.acceptClient();
-			for ( std::vector< pollfd >::iterator poll_it = irc._pfds.begin() + 1;
-				  poll_it != irc._pfds.end(); ++poll_it ) {
-				if ( poll_it->revents & POLLIN ) irc.recvMsg( *poll_it );
+			for ( int idx = 0; idx < irc._pfds.size(); ++idx ) {
+				if ( irc._pfds[ idx + 1 ].revents & ( POLLERR | POLLHUP | POLLNVAL ) ) {
+					irc.disconClient( idx );
+					continue;
+				}
+				if ( irc._pfds[ idx + 1 ].revents & POLLIN ) irc.recvMsg( idx );
+				if ( irc._pfds[ idx + 1 ].revents & POLLOUT ) irc.sendMsg( idx );
 			}
 		}
 		if ( close( mastersock.fd ) ) ERRNO_EXCEPT;
